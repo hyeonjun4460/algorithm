22a17d878e2328efe8b13dfbafe7596c5e2bee9f
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-04-17 18:00:25 +0900

    숫자만 추출: replace, 예외처리

---
+++ 탐색&시뮬레이션/숫자만 추출/AA.py
@@ -0,0 +1,32 @@
+import sys
+# sys.stdin = open('input.txt', 'rt')
+
+data = list(input())
+
+number = ''
+# 숫자만 빼기
+for i in range(len(data)):
+    try:
+        int(data[i])
+        number += data[i]
+    except ValueError:
+        continue
+
+answer = number
+# 앞의 0 제거하기
+for x in number:
+    if x == '0':
+        answer = answer.replace(x,'',1)
+    else:
+        break
+# 약수 개수 구하기
+cnt = 0
+for i in range(1,int(answer)+1):
+    if int(answer) % i == 0:
+        cnt+=1
+# 정답
+print(answer)
+print(cnt)
+
+# 
+    
