24119376a1070da290d7ff720cb5c330b34ae66a
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-04-24 18:08:33 +0900

    봉우리 문제 풀이: 2차원 배열 생성

---
+++ 탐색&시뮬레이션/봉우리/AA.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open('input.txt', 'rt')
+
+N = int(input())
+
+# 행/열 모두 인덱스 1부터 n-1까지
+
+# 상하좌우보다 값이 크면 cnt ++, 아니면  continue
+stage = list([0]*(N+2) for _ in range(N+2))
+
+for i in range(1, len(stage)-1):
+    stage[i][1:len(stage)-1] = map(int,input().split())
+
+answer = 0
+for i in range(1,len(stage)-1):
+    for j in range(1,len(stage)-1):
+        target = stage[i][j]
+        left = stage[i][j-1]
+        right = stage[i][j+1]
+        up = stage[i-1][j]
+        down = stage[i+1][j]
+        if target > left and target > right and target > up and target > down:
+            answer +=1
+        else:
+            continue
+print(answer)
+
