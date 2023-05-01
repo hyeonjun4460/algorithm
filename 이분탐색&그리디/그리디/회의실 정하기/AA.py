e278e2e11519cf62e454b8aee6bcbe2001bda510
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-05-01 20:05:46 +0900

    회의실 배정 문제 풀이 추가: sort에 우선순위부여 lambda (1,2) 이용

--- 이분탐색&그리디/그리디/회의실 정하기/AA.py
+++ 이분탐색&그리디/그리디/회의실 정하기/AA.py
@@ -4,7 +4,7 @@
 # 1. 회의가 끙나는 시간이 빠르다는건, 회의 시작 시간도 이르다는 것이다.
 # 2. 회의가 끝나는 시간을 오름차순 정렬하면, 최대한 많은 회의를 비교하고, 실행할 수 있다.
 import sys
-# sys.stdin = open('input.txt', 'rt')
+sys.stdin = open('input.txt', 'rt')
 
 n = int(input())
 
@@ -13,7 +13,8 @@ meetings = list()
 for _ in range(n):
     meetings.append(list(map(int, input().split())))
 
-meetings.sort(key= lambda x: x[1])
+meetings.sort(key= lambda x: (x[1],x[0]))
+print(meetings)
 # 그 다음 회의의 시작 시간이 이전 회의의 끝나는 시간보다 크거나 같으면 +, 끝나는 시간
 #  바로미터 바꾸기
 cnt = 1
