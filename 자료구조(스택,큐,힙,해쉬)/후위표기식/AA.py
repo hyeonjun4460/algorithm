7df78dd7bfb48f6daa7baaa79812ece56ba680aa
hyeonjun4460 <hyeonjun4460@gmail.com>
2023-05-26 16:32:00 +0900

    [스택&큐] 중위표기식 -> 후위표기식 풀이(상세내용) - ()는 따로 계산해서 식을 만든 뒤, )로 식이 끝나면 answer에 추가 - *,/는 operator에 appendleft로 추가, +,-는 append - () 밖에서 operator에 들어있는 최근 기호가 *,/면 answer에 추가 - 현재 기호가 +나 -인 경우, 이전 기호는 answer에 추가

--- 자료구조(스택,큐,힙,해쉬)/후위표기식/AA.py
+++ 자료구조(스택,큐,힙,해쉬)/후위표기식/AA.py
@@ -1,12 +1,64 @@
 import sys
 from collections import deque
-sys.stdin = open('input.txt', 'rt')
+# sys.stdin = open('input.txt', 'rt')
 formula = list(input())
 
 operators = deque()
 answer = ''
-tmp = False
-
-asdsadasdasd
+bracket_status = False
+bracket = ''
+bracket_operators = deque()
 # 우선순위가 높은 연산자
-# 큐에서 연산자를 꺼내야하는 상황에 대한 정리
+# 괄호 안에 있는 기호들
+# *, /
+# +, -
+
+# 숫자면 answer에 추가
+# 기호면 큐에 넣기
+# )가 끝나면 answer에 ++하기
+
+for x in formula:
+    if bracket_status == False:
+        if x != '+' and x != '-' and x != '*' and x != '/' and x != '(' and x !=')':
+            answer += x
+        else:
+            if x == '(':
+                # operator 싹 비우기
+                bracket_status = True
+                continue
+            if len(operators) != 0 and operators[0] != "+" and operators[0] != '-':
+                    answer += operators.popleft()
+            if x == '*' or x == '/':
+                operators.appendleft(x)
+            else:
+                if len(operators) !=0:
+                    answer+=operators.popleft()
+                operators.append(x)
+    else:
+        if x != '+' and x != '-' and x != '*' and x != '/' and x != '(' and x !=')':
+            bracket += x
+        else:
+            if x == ')':
+                while len(bracket_operators) != 0:
+                    bracket += bracket_operators.popleft()
+                # bracket operator 싹 비우기
+                answer += bracket
+                if len(operators) != 0 and operators[0] != "+" and operators[0] != '-':
+                    answer += operators.popleft()
+                bracket = ''
+
+                bracket_status = False
+                continue
+            if len(bracket_operators) != 0 and bracket_operators[0] != "+" and bracket_operators[0] != '-':
+                    answer += bracket_operators.popleft()
+            if x == '*' or x == '/':
+                bracket_operators.appendleft(x)
+            else:
+                if len(bracket_operators) !=0:
+                    bracket+=bracket_operators.popleft()
+                bracket_operators.append(x)
+else:
+    while len(operators) != 0:
+        answer += operators.popleft()
+
+print(answer)
